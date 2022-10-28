3e6dff245a77de9964ebae809dfc454f27a3fe7b
Soosi18 <sarvagya.pandey2000@yahoo.com>
2022-10-28 08:02:02 -0700

    add script for getting gentle output

---
+++ src/gentle_out.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import json
+
+
+URL = "http://localhost:8765/transcriptions?async=false"
+audioPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'audio', 'test.mp3'))
+textPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'text', 'test.txt'))
+audioFile = open(audioPath, "rb")
+textFile = open(textPath, "rb")
+phonemeList = requests.post(
+    url=URL,
+    files={
+        'audio': audioFile,
+        'transcript': textFile
+    }
+)
+phonemeList = phonemeList.json()
+print(json.dumps(phonemeList, indent=2))
+
+
+# just messing around from this point. feel free to delete
+words = phonemeList['words']
+for word in words:
+    print("word = " + word['alignedWord'] + "\nphonemes in word = ")
+    for phoneInfo in word['phones']:
+        print(phoneInfo['phone'] + ", ", end='')

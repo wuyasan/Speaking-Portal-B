0ba052fd04f0c29ea76c8f7cc00f868362386c50
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2022-11-04 15:06:51 -0700

    Multiple new scripts
    Added Python scheduler to create phoneme/image timeline correspondence
    added changes to gentle_out to generate a .JSON file with phoneme dict
    added videodrawer.py to create library of frames with lipsync and create mouthcoordinates.csv
    added videoFinisher.py to compile frames audio and background into video

--- src/gentle_out.py
+++ src/gentle_out.py
@@ -1,11 +1,14 @@
 import requests
 import os
 import json
+import pathlib
 
 
 URL = "http://localhost:8765/transcriptions?async=false"
-audioPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'audio', 'test.mp3'))
-textPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'text', 'test.txt'))
+audioPath = os.path.realpath(os.path.join(
+    os.path.dirname(__file__), '..', 'data', 'audio', 'test.mp3'))
+textPath = os.path.realpath(os.path.join(
+    os.path.dirname(__file__), '..', 'data', 'text', 'test.txt'))
 audioFile = open(audioPath, "rb")
 textFile = open(textPath, "rb")
 phonemeList = requests.post(
@@ -15,8 +18,14 @@ phonemeList = requests.post(
         'transcript': textFile
     }
 )
+
+
 phonemeList = phonemeList.json()
 print(json.dumps(phonemeList, indent=2))
+localpath = pathlib.Path(__file__).parent.resolve().parent.resolve()
+
+with open(str(localpath) + "\\data\\text\\test.json", "w") as outfile:
+    json.dump(phonemeList, outfile)
 
 
 # Splits up phoneme timestamp and duration
@@ -28,4 +37,7 @@ for word in words:
     for phoneInfo in word['phones']:
         print(phoneInfo['phone'] + ", ", end='')
         wstart += phoneInfo['duration']
-        print("Duration: " + str(phoneInfo['duration']) + " Phoneme end t-stamp: " + str(round(wstart,2)))
+        print("Duration: " + str(phoneInfo['duration']) +
+              " Phoneme end t-stamp: " + str(round(wstart, 2)))
+for word in words:
+    print(word['alignedWord'] + " ")

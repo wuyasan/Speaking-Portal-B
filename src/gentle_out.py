92e4a0391c49dcf8f0a54e904a1554534c2aa894
Soosi18 <sarvagya.pandey2000@yahoo.com>
2022-11-04 22:47:50 -0700

    Refactor for easier testing

--- src/gentle_out.py
+++ src/gentle_out.py
@@ -4,40 +4,59 @@ import json
 import pathlib
 
 
-URL = "http://localhost:8765/transcriptions?async=false"
-audioPath = os.path.realpath(os.path.join(
-    os.path.dirname(__file__), '..', 'data', 'audio', 'test.mp3'))
-textPath = os.path.realpath(os.path.join(
-    os.path.dirname(__file__), '..', 'data', 'text', 'test.txt'))
-audioFile = open(audioPath, "rb")
-textFile = open(textPath, "rb")
-phonemeList = requests.post(
-    url=URL,
-    files={
-        'audio': audioFile,
-        'transcript': textFile
-    }
-)
+def generate_phoneme_list(URL, text_path, audio_path):
+    try:
+        with open(audio_path, "rb") as audio_file, open(text_path, "rb") as text_file:
+            print("files loaded. generating phonemes...")
+            phoneme_list = requests.post(
+                url=URL,
+                files={
+                    'audio': audio_file,
+                    'transcript': text_file
+                }
+            )
+            print("generated list")
+            return phoneme_list
+    except Exception:
+        "Couldn't Open file(s)..."
 
 
-phonemeList = phonemeList.json()
-print(json.dumps(phonemeList, indent=2))
-localpath = pathlib.Path(__file__).parent.resolve().parent.resolve()
+def print_phoneme_list(phoneme_list):
+    phoneme_list = phoneme_list.json()
+    print(json.dumps(phoneme_list, indent=2))
 
-with open(str(localpath) + "\\data\\text\\test.json", "w") as outfile:
-    json.dump(phonemeList, outfile)
+
+def save_phoneme_list(local_path):
+    try:
+        with open(local_path, "w") as outfile:
+            json.dump(phoneme_list, outfile)
+    except Exception:
+        "Couldn't save file..."
 
 
 # Splits up phoneme timestamp and duration
-words = phonemeList['words']
-for word in words:
-    wstart = word['start']
-    print("\n\nWord start t-stamp = " + str(wstart))
-    print("word = " + word['alignedWord'] + "\nphonemes in word = \n", end='')
-    for phoneInfo in word['phones']:
-        print(phoneInfo['phone'] + ", ", end='')
-        wstart += phoneInfo['duration']
-        print("Duration: " + str(phoneInfo['duration']) +
-              " Phoneme end t-stamp: " + str(round(wstart, 2)))
-for word in words:
-    print(word['alignedWord'] + " ")
+def print_phoneme_sequence(phoneme_list):
+    phoneme_list = phoneme_list.json()
+    words = phoneme_list['words']
+    for word in words:
+        wstart = word['start']
+        print("\n\nWord start t-stamp = " + str(wstart))
+        print("word = " + word['alignedWord'] + "\nphonemes in word = \n", end='')
+        for phone_info in word['phones']:
+            print(phone_info['phone'] + ", ", end='')
+            wstart += phone_info['duration']
+            print("Duration: " + str(phone_info['duration']) + " Phoneme end t-stamp: " + str(round(wstart, 2)))
+
+
+if __name__ == '__main__':
+    URL = "http://localhost:8765/transcriptions?async=false"
+    audio_path = os.path.realpath(os.path.join(
+        os.path.dirname(__file__), '..', 'data', 'audio', 'test.wav'))
+    text_path = os.path.realpath(os.path.join(
+        os.path.dirname(__file__), '..', 'data', 'text', 'test.txt'))
+    local_path = pathlib.Path(__file__).parent.resolve().parent.resolve()
+    local_path = str(local_path) + "\\data\\text\\test.json"
+    phoneme_list = generate_phoneme_list(URL, text_path, audio_path)
+    print_phoneme_list(phoneme_list)
+    save_phoneme_list(local_path)
+    print_phoneme_sequence(phoneme_list)

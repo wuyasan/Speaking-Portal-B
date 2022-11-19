e8f1afd7e935efd3a553f552e2673f4954f3ddf1
wuyasan <youtosan0@gmail.com>
2022-11-19 00:44:10 -0500

    Update GUI related code

--- src/GUI_Process/GUI.py
+++ src/GUI_Process/GUI.py
@@ -1,8 +1,10 @@
+import imp
 from stringprep import b3_exceptions
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import os
+from gentle_func import geneGentle
 
 # create root window
 root = Tk()
@@ -13,33 +15,41 @@ root.geometry('500x500')
 
 label = Label(root, text="Get your file", font=("Courier 10"))
 
+textname = ""
+audioname = ""
 
-def browsetxtFile():
-    filename = filedialog.askopenfilename(initialdir="/",
+def run():
+    os.system('python docker_script.py')
+    geneGentle(textname, audioname)
+
+def browseTxtFile():
+    textname = filedialog.askopenfilename(initialdir="/",
                                           title="Select a File",
                                           filetypes=(("Text files",
                                                       "*.txt*"),
                                                      ("All files",
                                                       "*.*")))
-    label.configure(text="File Opened: "+filename)
+    label.configure(text="File Opened: "+textname)
 
-def browseaudioFile():
-    filename = filedialog.askopenfilename(initialdir="/",
+def browseAudioFile():
+    audioname = filedialog.askopenfilename(initialdir="/",
                                           title="Select a File",
                                           filetypes=(("Audio Files",
                                                       "*.mp3*"),
                                                      ("All files",
                                                       "*.*")))
-    label.configure(text="File Opened: "+filename)
+    label.configure(text="File Opened: "+audioname)
 
-b_t = Button(root, text="select text file", width=20, command=browsetxtFile)
-b_a = Button(root, text="select audio file", width=20, command=browseaudioFile)
+b_t = Button(root, text="select text file", width=20, command=browseTxtFile)
+b_a = Button(root, text="select audio file", width=20, command=browseAudioFile)
 b_e = Button(root, text="exit", width=20, command=exit)
+b_r = Button(root, text="get the video", width=20, command=run)
 
 label.grid(column=5, row=1)
 b_t.grid(column=5, row=2)
 b_a.grid(column=5, row=3)
-b_e.grid(column=5, row=4)
+b_r.grid(column=5, row=4)
+b_e.grid(column=5, row=5)
 # all widgets will be here
 # Execute Tkinter
 root.mainloop()

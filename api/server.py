fbf3592e0b412eeb24374716f81bcecbb711749d
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2022-11-23 17:21:10 -0800

    bare bones route setup

--- api/server.py
+++ api/server.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/helloworld', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/text-to-speech', methods=['POST'])
+def text_to_speech():
+    return 'This will return the audio file'
+
+@app.route('/gentle-output', methods=['POST'])
+def gentle_output():
+    return 'This will return the gentle output'
+
+@app.route('/generate-video', methods=['GET'])
+def video_processing():
+    return 'This will return the video'
+
+@app.route('/get-avatar', methods=['GET'])
+def get_avatar():
+    return 'This will return the avatar'

997c8db85201fb964bc6de90f01086d06d4b434c
Soosi18 <sarvagya.pandey2000@yahoo.com>
2022-11-26 02:14:08 -0800

    GUI.py actually generates video now. Also, some minor layout changes

--- src/GUI.py
+++ src/GUI.py
@@ -10,65 +10,71 @@ import json
 import pathlib
 import requests
 from gentle_out import *
+from videoFinisher import *
 
 
 # create root window
 root = Tk()
 # root window title and dimension
-root.title("Put the text in and audio out")
+root.title("Speaking Portal")
 # Set geometry (widthxheight)
 root.geometry('500x500')
-
-label = Label(root, text="Get your file", font=("Courier 10"))
+# launch docker container
+os.system("python docker_script.py")
 
 textPath = ""
 audioPath = ""
+local_path = pathlib.Path(__file__).parent.resolve().parent.resolve()
+sys_info = platform.system()
+if sys_info == "Darwin":
+    local_path = str(local_path) + "/data/text/test.json"
+else:
+    local_path = str(local_path) + "\\data\\text\\test.json"
 
 def browseTxtFile():
     global textPath
-    textPath = filedialog.askopenfilename(initialdir="/",
+    textPath = filedialog.askopenfilename(initialdir=localpath,
                                           title="Select a File",
                                           filetypes=(("Text files",
                                                       "*.txt*"),
                                                      ("All files",
                                                       "*.*")))
-    label.configure(text="File Opened: "+textPath)
+    label.configure(text="Text File Loaded!")
 
 def browseAudioFile():
     global audioPath
-    audioPath = filedialog.askopenfilename(initialdir="/",
+    audioPath = filedialog.askopenfilename(initialdir=localpath,
                                           title="Select a File",
                                           filetypes=(("Audio Files",
-                                                      "*.mp3*"),
+                                                      "*.mp3*, *.wav*"),
                                                      ("All files",
                                                       "*.*")))
-    label.configure(text="File Opened: "+audioPath)
+    label.configure(text="Audio File Loaded!")
 
 def run():
     print(textPath + " " + audioPath)
     if __name__ == '__main__':
         URL = "http://localhost:8765/transcriptions?async=false"
-        local_path = pathlib.Path(__file__).parent.resolve().parent.resolve()
-        sys_info = platform.system()
-        if sys_info == "Darwin":
-            local_path = str(local_path) + "/data/text/test.json"
-        else:
-            local_path = str(local_path) + "\\data\\text\\test.json"
         phoneme_list = generate_phoneme_list(URL, textPath, audioPath)
-        print_phoneme_list(phoneme_list)
-        save_phoneme_list(local_path)
-        print_phoneme_sequence(phoneme_list)
+        save_phoneme_list(local_path, phoneme_list)
+        generate_video(audioPath, textPath)
+        exit
 
-b_t = Button(root, text="select text file", width=20, command=browseTxtFile)
-b_a = Button(root, text="select audio file", width=20, command=browseAudioFile)
-b_e = Button(root, text="exit", width=20, command=exit)
-b_r = Button(root, text="get the video", width=20, command=run)
+label = Label(root, text="", font=("Courier 10 bold"))
+heading = Label(root, text="Speaking Portal - Video Generator", font=("Courier 16"), bg="white", width=65, height=5)
 
-label.grid(column=5, row=1)
-b_t.grid(column=5, row=2)
-b_a.grid(column=5, row=3)
-b_r.grid(column=5, row=4)
-b_e.grid(column=5, row=5)
+b_t = Button(root, text="select text file", width=30, height=2, command=browseTxtFile)
+b_a = Button(root, text="select audio file", width=30, height=2, command=browseAudioFile)
+b_e = Button(root, text="exit", width=30, height=2, command=exit)
+b_r = Button(root, text="Generate Video", width=30, height=2, command=run)
+
+root.columnconfigure(1, weight=1)
+label.grid(column=1, row=2)
+heading.grid(column=1, row=1)
+b_t.grid(column=1, row=3, columnspan=2)
+b_a.grid(column=1, row=4)
+b_r.grid(column=1, row=5)
+b_e.grid(column=1, row=6)
 # all widgets will be here
 # Execute Tkinter
 root.mainloop()

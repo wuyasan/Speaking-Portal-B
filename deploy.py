e1cc71529ce411f1433d51f469fbd35081b361ac
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2022-11-30 09:29:56 -0800

    filepath fixes, print logs and update to deploy.py

--- deploy.py
+++ deploy.py
@@ -22,30 +22,93 @@ if active_account != GCLOUD_ACCOUNT:
 
 # Dry Run
 
-cwd = os.getcwd() + '/src'
+cwd = os.getcwd()
 
-os.chdir(cwd)
+# Parse all files
 
-print("In dir: " + os.getcwd())
+# Parse Root Directory
+rootFiles = []
+exclude = ['.git', 'docs', 'api', 'mouths', 'backgrounds', 'poses', 'data', 'src']
+for root, dirs, files in os.walk(cwd):
+    dirs[:] = [d for d in dirs if d not in exclude]
+    for name in files:
+        rootFiles.append(os.path.join(root, name))
+
+print("\n")
+    # for name in dirs:
+    #     print("DIRS: " + os.path.join(root, name))
+
+exclude = ['.git']
+# Parse Mouths Directory
+mouthFiles = []
+
+for root, dirs, files in os.walk(cwd + '/mouths'):
+    for name in files:
+        print(name)
+        mouthFiles.append(os.path.join(root, name))
+
+print("\n")
+# Parse Backgrounds Directory
+backgroundFiles = []
+
+for root, dirs, files in os.walk(cwd + '/backgrounds'):
+    for name in files:
+        backgroundFiles.append(os.path.join(root, name))
+
+print("\n")
+# Parse Poses Directory
+poseFiles = []
+
+for root, dirs, files in os.walk(cwd + '/poses'):
+    for name in files:
+        poseFiles.append(os.path.join(root, name))
+
+print("\n")
+# Parse Data Directory
+dataFiles = []
+
+for root, dirs, files in os.walk(cwd + '/data'):
+    for name in files:
+        dataFiles.append(os.path.join(root, name))
+
+print("\n")
+# Parse Src Directory
+srcFiles = []
+
+for root, dirs, files in os.walk(cwd + '/src'):
+    print("SRC FILES: ")
+    for name in files:
+        srcFiles.append(os.path.join(root, name))
+
+print("\n")
+# Parse api Directory
+apiFiles = []
+
+for root, dirs, files in os.walk(cwd + '/api'):
+    print("API FILES: ")
+    for name in files:
+        apiFiles.append(os.path.join(root, name))
 
 # Loop through all files in the src and save paths to a list
 
-files = []
-for file in os.listdir(os.getcwd()):
-    if file.endswith(".py"):
-        files.append(os.path.join(os.getcwd(), file))
+# files = []
+# for file in os.listdir(os.getcwd()):
+#     # if file.endswith(".py"):
+#     files.append(os.path.join(os.getcwd(), file))
 
-print("Files to be deployed: " + str(files))
+# print("Files to be deployed: ")
 
-# Deploy files to GCP
+# for file in rootFiles:
+#     print(file)
+# # Deploy files to GCP
 
 file_str = ' '.join(files)
-print("Deploying files: " + file_str)
-upload_file_cmd = 'gcloud compute scp "{}" {}:Speaking-Portal-B --zone={} --project={}'.format(file_str, VM_NAME, ZONE, PROJECT)
+# print("Deploying files: " + file_str)
+# upload_file_cmd = 'gcloud compute scp "{}" {}:Speaking-Portal-B --zone={} --project={}'.format(file_str, VM_NAME, ZONE, PROJECT)
 
 # The first you run this command it will ask to generate a SSH key file
 
-pipe = os.popen(upload_file_cmd) # Will ask for ssh passphrase everytime it is run
+# pipe = os.popen(upload_file_cmd) # Will ask for ssh passphrase everytime it is run
 
 print("Files uploaded")
 

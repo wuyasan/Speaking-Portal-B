a43b7de4bd9808ef0aba006a12f430691ba7fb42
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2023-04-05 09:39:24 -0700

    Add files via upload

---
+++ src/vm_docker_installation.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This script should be run on the VM.
+
+# Install Docker
+sudo apt update
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
